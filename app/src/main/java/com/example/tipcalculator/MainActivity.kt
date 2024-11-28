bd7fd7d6bbd010a57170386a742ee790628875dc
agwenchez <agweraenock76@gmail.com>
2024-11-28 21:13:46 +0300

    UI update changes

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -24,26 +23,25 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 import java.text.NumberFormat
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +49,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TipCalculatorTheme {
-                Surface(modifier = Modifier.fillMaxSize()
+                Surface(
+                    modifier = Modifier.fillMaxSize()
                 ) {
                     TipCalculatorLayout()
                 }
@@ -70,20 +69,19 @@ fun TipCalculatorLayout() {
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val tip = calculateTip(amount, tipPercent, roundUp)
 
-
     Column(
         modifier = Modifier
             .statusBarsPadding()
-            .padding(horizontal = 40.dp, vertical = 32.dp)
-            .verticalScroll(rememberScrollState())
-            .safeDrawingPadding(),
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState()),
+//            .safeDrawingPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             stringResource(R.string.calculate_tip),
             modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
+                .padding(bottom = 10.dp, top = 16.dp)
                 .align(alignment = Alignment.Start)
         )
         EditNumberField(
@@ -118,7 +116,7 @@ fun TipCalculatorLayout() {
             roundUp = roundUp,
             onRoundUpChanged = { roundUp = it}
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
         Text(
             stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
@@ -161,7 +159,7 @@ fun EditNumberField(
       modifier: Modifier = Modifier,
       keyboardOptions: KeyboardOptions
 ){
-    TextField(
+    OutlinedTextField(
         value,
         onValueChange,
         singleLine = true,
@@ -181,7 +179,7 @@ fun EditNumberField(
 internal fun calculateTip(amount: Double, tipPercent: Double = 15.0,  roundUp: Boolean) : String{
     var tip = tipPercent / 100 * amount
     if(roundUp){
-        tip = kotlin.math.ceil(amount)
+        tip = kotlin.math.ceil(tipPercent / 100 * amount)
     }
 //    convert to comma separated values like in currencies
     return NumberFormat.getCurrencyInstance().format(tip)
